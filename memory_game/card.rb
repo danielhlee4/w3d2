257268202f80b6d996736a8e37d15298be7acfed
Vincent <vshan0816@gmail.com>
2023-07-11 13:57:18 -0400

    face up start

--- memory_game/card.rb
+++ memory_game/card.rb
@@ -2,8 +2,14 @@ class Card
     attr_reader :face_value
     def initialize(face_value)
         @face_value = face_value
-        @orientation = false
+        @face_up = false
     end
 
-
+    def switch_orientation
+        if @face_up == false
+            @face_up = true
+        else
+            @face_up = false
+        end
+    end
 end
